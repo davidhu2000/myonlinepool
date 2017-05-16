c5381dd184513442c6e43f3f1c4e883b3287c0be
Alex Sherman <asherman.ca@gmail.com>
2017-05-16 13:10:29 -0700

    font and navbar components added

--- frontend/core/navbar/navbar.jsx
+++ frontend/core/navbar/navbar.jsx
@@ -8,9 +8,15 @@ class Navbar extends React.Component {
   render() {
     return (
       <div className='navbar-container'>
-        <button>settings</button>
-        <p>My Online Pool</p>
-        <button>account</button>
+        <button>
+          <i className="fa fa-angle-down" aria-hidden="true"></i>
+          Settings
+        </button>
+        <h1>My Online Pool</h1>
+        <button>
+          <i className="fa fa-angle-down" aria-hidden="true"></i>
+          Account
+        </button>
       </div>
     );
   }
