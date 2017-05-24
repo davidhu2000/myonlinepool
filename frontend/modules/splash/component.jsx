7291948374221ccfff96f6118e23f9bce17681bc
Alex Sherman <asherman.ca@gmail.com>
2017-05-24 16:01:23 -0700

    cleaned up splash containers

--- frontend/modules/splash/component.jsx
+++ frontend/modules/splash/component.jsx
@@ -9,17 +9,21 @@ class Splash extends React.Component {
   render() {
     return (
       <div className="splash-container">
-        <div className="splash-left">
-          <h1>Welcome to My Online Pool!</h1>
-          <div className="splash-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-          <div className="splash-bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+        <div className="splash-title">
+          Welcome to My Online Pool!
         </div>
-        <div className="splash-right">
-          <img src="assets/cardinals.png"/>
-          <img src="assets/ravens.png"/>
-          <img src="assets/bears.png"/>
-          <img src="assets/bills.png"/>
-          <img src="assets/panthers.png"/>
+        <div className="splash-body">
+          <div className="splash-left">
+            <div className="splash-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            <div className="splash-bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </div>
+          <div className="splash-right">
+            <img src="assets/cardinals.png"/>
+            <img src="assets/ravens.png"/>
+            <img src="assets/bears.png"/>
+            <img src="assets/bills.png"/>
+            <img src="assets/panthers.png"/>
+          </div>
         </div>
       </div>
     );
