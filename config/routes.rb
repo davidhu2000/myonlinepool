709a645649aed878ef018c67dc97f37691b1b9d8
David Hu <davidhu314@gmail.com>
2017-05-25 11:07:45 -0700

    generate auth related controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do 
-    devise_for :users
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
   end
   root "static_pages#root"
 end
