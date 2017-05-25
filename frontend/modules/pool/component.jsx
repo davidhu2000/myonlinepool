ebb0f109d8c8242bca99a49e3a085bb8cb84ace1
Alex Sherman <asherman.ca@gmail.com>
2017-05-24 18:46:23 -0700

    splash pool transplant

--- frontend/modules/pool/component.jsx
+++ frontend/modules/pool/component.jsx
@@ -9,35 +9,20 @@ class Pool extends React.Component {
   render() {
     return (
       <div className="pool-container">
-        <div className="pool-top">
-          <h1>Welcome to<span> My Online Pool</span> </h1>
-          <p>
-          A simple app to organize your betting pools.
-          <br/>
-          No more organizational nightmares.
-          </p>
+        <div className="pool-title">
+          Welcome to My Online Pool!
         </div>
-        <div className="pool-mid">
-          <h1>Play with your favorite teams</h1>
-          <div className="pool-mid-carousel">
-            <img className="shield" src="assets/shield.svg"/>
+        <div className="pool-body">
+          <div className="pool-left">
+            <div className="pool-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+            <div className="pool-bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+          </div>
+          <div className="pool-right">
+            <img src="assets/shield.svg"/>
             <img src="assets/cardinals.png"/>
             <img src="assets/bears.png"/>
             <img src="assets/bills.png"/>
             <img src="assets/panthers.png"/>
-          </div>
-        </div>
-        <div className="pool-bottom">
-          <h1>Create or join pools with all of your groups</h1>
-          <div className="pool-bottom-body">
-            <p>Work</p>
-            <p>Family</p>
-            <p>Friends</p>
-          </div>
-          <div className="pool-bottom-body">
-            <p>Enemies</p>
-            <p>Frenemies</p>
-            <p>Strangers</p>
           </div>
         </div>
       </div>
--- frontend/modules/splash/component.jsx
+++ frontend/modules/splash/component.jsx
@@ -9,20 +9,35 @@ class Splash extends React.Component {
   render() {
     return (
       <div className="splash-container">
-        <div className="splash-title">
-          Welcome to My Online Pool!
+        <div className="splash-top">
+          <h1>Welcome to<span> My Online Pool</span> </h1>
+          <p>
+          A simple app to organize your sports pools.
+          <br/>
+          No more organizational nightmares.
+          </p>
         </div>
-        <div className="splash-body">
-          <div className="splash-left">
-            <div className="splash-top">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-            <div className="splash-bottom">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-          </div>
-          <div className="splash-right">
-            <img src="assets/shield.svg"/>
+        <div className="splash-mid">
+          <h1>Play with your favorite teams</h1>
+          <div className="splash-mid-carousel">
+            <img className="shield" src="assets/shield.svg"/>
             <img src="assets/cardinals.png"/>
             <img src="assets/bears.png"/>
             <img src="assets/bills.png"/>
             <img src="assets/panthers.png"/>
+          </div>
+        </div>
+        <div className="splash-bottom">
+          <h1>Create or join pools with all of your groups</h1>
+          <div className="splash-bottom-body">
+            <p>Work</p>
+            <p>Family</p>
+            <p>Friends</p>
+          </div>
+          <div className="splash-bottom-body">
+            <p>Enemies</p>
+            <p>Frenemies</p>
+            <p>Strangers</p>
           </div>
         </div>
       </div>
