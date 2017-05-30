37154ae25114465b0bc53613873f009f6dcf0869
David Hu <davidhu314@gmail.com>
2017-05-30 15:03:50 -0700

    add column to game_nfl, add one week of test schedule

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,14 @@ require 'roo'
 
 data = Roo::Spreadsheet.open('db/seed/seed.xlsx')
 
+# seed a basic user
+mainAdminUser = User.new(
+    email: "me@gmail.com",
+    password: 'password'
+)
+mainAdminUser.skip_confirmation!
+mainAdminUser.save!
+
 # seed the database with NFL team information
 data.sheet('team_nfl').each_with_index do |team, idx|
   next if idx.zero?
@@ -10,5 +18,20 @@ data.sheet('team_nfl').each_with_index do |team, idx|
     name: team[1].downcase,
     abbreviation: team[2].downcase, 
     league: team[3].downcase
+  )
+end
+
+# seed test schedule
+data.sheet('test_schedule').each_with_index do |game, idx|
+  next if idx.zero?
+  GameNfl.create!(
+    season: game[0],
+    week: game[1],
+    home_id: game[2],
+    away_id: game[3],
+    home_score: game[4],
+    away_score: game[5],
+    completed: game[6],
+    start_time: game[7]
   )
 end
