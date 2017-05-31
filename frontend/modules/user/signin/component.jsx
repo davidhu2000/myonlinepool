aaf85b18b14d9f998ef1ed3512a25dab65a449b0
Alex Sherman <asherman.ca@gmail.com>
2017-05-31 12:49:39 -0700

    auth forms

--- frontend/modules/user/signin/component.jsx
+++ frontend/modules/user/signin/component.jsx
@@ -4,12 +4,45 @@ import { withRouter, Link } from 'react-router';
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.submitUser = this.submitUser.bind(this);
+  }
+
+  submitUser() {
+    e.preventDefault();
+  }
+
+  update(field) {
+    return e => {
+      this.setState({
+        [field]: e.target.value
+      });
+    };
   }
 
   render() {
     return (
-      <div className="signin-container">
-        Sign In
+      <div className="signup-container">
+        <form onSubmit={ this.submitUSer } className="auth-form">
+          <input  name="username"
+                  placeholder="Username"
+                  value={ this.state.username }
+                  onChange={ this.update("username") }
+                  className="auth-form-name"></input>
+          <input  name="password"
+                  placeholder="Password"
+                  value={ this.state.password }
+                  onChange={ this.update("password") }
+                  className="auth-form-input"></input>
+          <input type='submit'
+                 className="auth-form-button"
+                 value="Sign In"></input>
+        </form>
       </div>
     );
   }
--- frontend/modules/user/signup/component.jsx
+++ frontend/modules/user/signup/component.jsx
@@ -4,12 +4,45 @@ import { withRouter, Link } from 'react-router';
 class SignUp extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.submitUser = this.submitUser.bind(this);
+  }
+
+  submitUser() {
+    e.preventDefault();
+  }
+
+  update(field) {
+    return e => {
+      this.setState({
+        [field]: e.target.value
+      });
+    };
   }
 
   render() {
     return (
       <div className="signup-container">
-        Sign Up
+        <form onSubmit={ this.submitUSer } className="auth-form">
+          <input  name="username"
+                  placeholder="Username"
+                  value={ this.state.username }
+                  onChange={ this.update("username") }
+                  className="auth-form-name"></input>
+          <input  name="password"
+                  placeholder="Password"
+                  value={ this.state.password }
+                  onChange={ this.update("password") }
+                  className="auth-form-input"></input>
+          <input type='submit'
+                 className="auth-form-button"
+                 value="Sign Up"></input>
+        </form>
       </div>
     );
   }
