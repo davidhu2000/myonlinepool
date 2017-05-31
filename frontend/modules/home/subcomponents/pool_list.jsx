a6396dad36957348e5abaf2a0b273cd2c594fcc9
Alex Sherman <asherman.ca@gmail.com>
2017-05-30 22:35:11 -0700

    pool list buttons

--- frontend/modules/home/subcomponents/pool_list.jsx
+++ frontend/modules/home/subcomponents/pool_list.jsx
@@ -5,7 +5,16 @@ import PoolListItem from './pool_list_item';
 class PoolList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showJoin: false,
+      joinName: "",
+      joinPass: ""
+    };
+
     this.genList = this.genList.bind(this);
+    this.toggleJoin = this.toggleJoin.bind(this);
+    this.submitJoin = this.submitJoin.bind(this);
   }
 
   genList() {
@@ -18,14 +27,58 @@ class PoolList extends React.Component {
     ));
   }
 
+  toggleJoin() {
+      this.setState({ showJoin: !this.state.showJoin });
+  }
+
+  submitJoin(e) {
+    e.preventDefault();
+  }
+
+  update(field) {
+    return e => {
+      this.setState({
+        [field]: e.target.value
+      });
+    };
+  }
+
   render() {
     return (
       <div className="pool-list">
         <div className="pool-list-top-item">{this.props.Title}</div>
         {this.genList()}
-        <form className="pool-form">
+        <button className="pool-join-button" onClick={this.toggleJoin.bind(this)}>
+        { this.state.showJoin ? <i
+          className="fa fa-minus"
+          aria-hidden="true"/> : <i
+          className="fa fa-plus"
+          aria-hidden="true"/> }
+          Join Pool
+        </button>
+        <button className="pool-create-button" onClick={this.toggleJoin.bind(this)}>
+        { this.state.showJoin ? <i
+          className="fa fa-minus"
+          aria-hidden="true"/> : <i
+          className="fa fa-plus"
+          aria-hidden="true"/> }
+          Create Pool
+        </button>
 
-        </form>
+        { this.state.showJoin ?
+          <form onSubmit={ this.submitJoin } className="pool-form">
+            <input  name="joinName"
+                    value={ this.state.joinName }
+                    onChange={ this.update("joinName") }
+                    className="pool-form-name"></input>
+            <input  name="joinPass"
+                    value={ this.state.joinPass }
+                    onChange={ this.update("joinPass") }
+                    className="pool-form-input"></input>
+            <input type='submit'
+                   className="pool-form-button"
+                   value="submit"></input>
+          </form> : null }
       </div>
     );
   }
