a81d61892f4ec46ee42c753d20dd0c3870fcf371
Alex Sherman <asherman.ca@gmail.com>
2017-05-30 22:54:31 -0700

    pool list forms

--- frontend/modules/home/subcomponents/pool_list.jsx
+++ frontend/modules/home/subcomponents/pool_list.jsx
@@ -8,6 +8,7 @@ class PoolList extends React.Component {
 
     this.state = {
       showJoin: false,
+      showCreate: false,
       joinName: "",
       joinPass: ""
     };
@@ -15,6 +16,8 @@ class PoolList extends React.Component {
     this.genList = this.genList.bind(this);
     this.toggleJoin = this.toggleJoin.bind(this);
     this.submitJoin = this.submitJoin.bind(this);
+    this.toggleCreate = this.toggleCreate.bind(this);
+    this.submitCreate = this.submitCreate.bind(this);
   }
 
   genList() {
@@ -28,10 +31,26 @@ class PoolList extends React.Component {
   }
 
   toggleJoin() {
+    if (this.state.showCreate) {
+      this.setState({ showCreate: false, showJoin: !this.state.showJoin })
+    } else {
       this.setState({ showJoin: !this.state.showJoin });
+    }
+  }
+
+  toggleCreate() {
+    if (this.state.showJoin) {
+      this.setState({ showJoin: false, showCreate: !this.state.showCreate })
+    } else {
+      this.setState({ showCreate: !this.state.showCreate });
+    }
   }
 
   submitJoin(e) {
+    e.preventDefault();
+  }
+
+  submitCreate(e) {
     e.preventDefault();
   }
 
@@ -49,24 +68,28 @@ class PoolList extends React.Component {
         <div className="pool-list-top-item">{this.props.Title}</div>
         {this.genList()}
         <button className="pool-join-button" onClick={this.toggleJoin.bind(this)}>
-        { this.state.showJoin ? <i
-          className="fa fa-minus"
-          aria-hidden="true"/> : <i
-          className="fa fa-plus"
-          aria-hidden="true"/> }
+          <div>
+          { this.state.showJoin ? <i
+            className="fa fa-minus"
+            aria-hidden="true"/> : <i
+            className="fa fa-plus"
+            aria-hidden="true"/> }
           Join Pool
+          </div>
         </button>
-        <button className="pool-create-button" onClick={this.toggleJoin.bind(this)}>
-        { this.state.showJoin ? <i
-          className="fa fa-minus"
-          aria-hidden="true"/> : <i
-          className="fa fa-plus"
-          aria-hidden="true"/> }
+        <button className="pool-create-button" onClick={this.toggleCreate.bind(this)}>
+          <div>
+          { this.state.showCreate ? <i
+            className="fa fa-minus"
+            aria-hidden="true"/> : <i
+            className="fa fa-plus"
+            aria-hidden="true"/> }
           Create Pool
+          </div>
         </button>
 
         { this.state.showJoin ?
-          <form onSubmit={ this.submitJoin } className="pool-form">
+          <form onSubmit={ this.submitJoin } className="pool-join-form">
             <input  name="joinName"
                     value={ this.state.joinName }
                     onChange={ this.update("joinName") }
@@ -79,6 +102,22 @@ class PoolList extends React.Component {
                    className="pool-form-button"
                    value="submit"></input>
           </form> : null }
+
+        { this.state.showCreate ?
+          <form onSubmit={ this.submitCreate } className="pool-create-form">
+            <input  name="createName"
+                    value={ this.state.createName }
+                    onChange={ this.update("createName") }
+                    className="pool-form-name"></input>
+            <input  name="createPass"
+                    value={ this.state.createPass }
+                    onChange={ this.update("createPass") }
+                    className="pool-form-input"></input>
+            <input type='submit'
+                   className="pool-form-button"
+                   value="submit"></input>
+          </form> : null }
+
       </div>
     );
   }
