f6a06fea063663b46aca73e4f8b99ba82da46311
David Hu <davidhu314@gmail.com>
2017-06-05 15:46:20 -0700

    update routes

--- frontend/core/routes.jsx
+++ frontend/core/routes.jsx
@@ -2,33 +2,35 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Route, IndexRoute} from 'react-router';
 import App from './app';
-import SplashContainer from 'modules/splash/';
-import HomeContainer from 'modules/home/';
-import PicksContainer from 'modules/picks/';
-import MetricsContainer from 'modules/metrics';
-import LeaderboardContainer from 'modules/leaderboard';
-import ModeratorContainer from 'modules/moderator';
-import PoolContainer from 'modules/pool';
-// import SigninContainer from 'modules/user/signin';
-import SignupContainer from 'modules/user';
+import Splash from 'modules/splash/';
+import Home from 'modules/home/';
+import Picks from 'modules/picks/';
+import Metrics from 'modules/metrics';
+import Leaderboard from 'modules/leaderboard';
+import Moderator from 'modules/moderator';
+import Pool from 'modules/pool';
+import Auth from 'modules/user';
+import PoolForm from 'modules/pool_form';
 
 const routes = (
   <Route path="/" component={App}>
-    <IndexRoute component={SplashContainer}/> 
+    <IndexRoute component={Splash} /> 
+
+    <Route path="/pool/create" component={PoolForm} />
     
-    <Route path="/signin" component={ SignupContainer }>
+    <Route path="/signin" component={Auth}>
     </Route>
 
-    <Route path="/signup" component={SignupContainer}></Route>
+    <Route path="/signup" component={Auth}></Route>
 
-    <Route path="/home" component={HomeContainer}></Route>
+    <Route path="/home" component={Home}></Route>
 
     <Route path="/pool/:poolId">
-      <IndexRoute component={PoolContainer} />
-      <Route path="picks" component={PicksContainer}></Route>
-      <Route path="leaderboard" component={LeaderboardContainer}></Route>
-      <Route path="metrics" component={MetricsContainer}></Route>
-      <Route path="moderator" component={ModeratorContainer}></Route>
+      <IndexRoute component={Pool} />
+      <Route path="picks" component={Picks}></Route>
+      <Route path="leaderboard" component={Leaderboard}></Route>
+      <Route path="metrics" component={Metrics}></Route>
+      <Route path="moderator" component={Moderator}></Route>
     </Route>
 
   </Route>
