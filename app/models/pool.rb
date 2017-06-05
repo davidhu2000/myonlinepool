ae7825f58a4d9426bdf8ec542b20c842c1d24b81
David Hu <davidhu314@gmail.com>
2017-06-05 13:24:30 -0700

    add basic model auth to pool

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -20,6 +20,8 @@ class Pool < ApplicationRecord
   validates :buy_in, presence: true, numericality: { only_integer: true }
   validates :league, presence: true
   validates :season, presence: true, numericality: { only_integer: true }
+  validates :password_digest, presence: { message: 'Password cannot be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
 
   belongs_to :moderator, foreign_key: :moderator_id, class_name: :User, primary_key: :id
 
@@ -30,4 +32,21 @@ class Pool < ApplicationRecord
   has_many :picks
   has_many :messages
   has_many :bulletins
+
+  def self.find_by_credentials(title, password)
+    pool = Pool.find_by(title: title)
+    pool && pool.valid_password?(password) ? pool : nil
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
 end
