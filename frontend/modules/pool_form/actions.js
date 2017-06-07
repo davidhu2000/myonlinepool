2cd44a74c530229c410e0892228b1868e33cb31d
David Hu <davidhu314@gmail.com>
2017-06-07 09:35:19 -0700

    create actions to create and receive pool

---
+++ frontend/modules/pool_form/actions.js
@@ -0,0 +1,15 @@
+import * as PoolFormAPI from './utils';
+
+export const RECEIVE_POOL = 'RECEIVE_POOL';
+
+export const receivePool = pool => ({
+  type: RECEIVE_POOL,
+  pool
+});
+
+export const createPool = pool => dispatch => (
+  PoolFormAPI.createPool(pool).then(
+    res => dispatch(receivePool(res)),
+    err => console.log(err)
+  )
+);
