47fd2dcc4fcf5a656c351ab141507c22789860d6
David Hu <davidhu314@gmail.com>
2017-06-07 09:30:38 -0700

    create basic form to create pool

--- frontend/modules/pool_form/component.jsx
+++ frontend/modules/pool_form/component.jsx
@@ -16,11 +16,55 @@ class PoolForm extends React.Component {
     autoBind(this);
   }
 
+  update(type) {
+    return e => {
+      this.setState({ [type]: e.target.value });
+    };
+  }
+
+  createPool(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className='pool-form'>
-        <form>
-          
+        <form onSubmit={this.createPool} >
+          <div className="">
+            <input
+              type="text"
+              className=""
+              value={this.state.title}
+              onChange={this.update('title')}
+            />
+            <label htmlFor="title">Title</label>
+          </div>
+
+          <div className="">
+            <textarea
+              type="text"
+              className=""
+              value={this.state.description}
+              onChange={this.update('description')}
+            />
+            <label htmlFor="description">Description</label>
+          </div>
+
+          <div className="">
+            <input
+              type="number"
+              className=""
+              value={this.state.buy_in}
+              onChange={this.update('buy_in')}
+            />
+            <label htmlFor="buy_in">Buy In</label>
+          </div>
+
+          <div className="">
+            <input id="buy_in" type="submit" className="" value="Create Pool" />
+          </div>
+
         </form>
       </div>
     );
