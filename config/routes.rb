ff3532d91822f19368efb2c5e30bb16329f6fd93
David Hu <davidhu314@gmail.com>
2017-06-09 13:03:57 -0700

    update routes for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  scope :api, default: { format: :json } do 
-    devise_for :users
-  end
+  # scope :api, default: { format: :json } do 
+  #   devise_for :users
+  # end
 
   namespace :api, default: { format: :json } do 
+    devise_for :users, controllers: {
+      sessions: 'api/users/sessions',
+      passwords: 'api/users/passwords',
+      registrations: 'api/users/registrations',
+      confirmations: 'api/users/confirmations',
+      unlocks: 'api/users/unlocks'
+    }
     resources :pools, only: [:show, :create, :destroy, :update]
   end
   root "static_pages#root"
