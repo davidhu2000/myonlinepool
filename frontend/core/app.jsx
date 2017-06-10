642408b7a6ff80b47d6abd9bbf975bb5202a53c6
David Hu <davidhu314@gmail.com>
2017-06-09 17:16:11 -0700

    add redirect in app.jsx

--- frontend/core/app.jsx
+++ frontend/core/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, Link } from 'react-router';
+import PropTypes from 'prop-types';
+
 import Footer from './footer';
 import Navbar from './navbar';
 
@@ -9,7 +11,32 @@ class App extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    this._redirect(this.props.loggedIn);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.props.loggedIn !== newProps.loggedIn) {
+      this._redirect(newProps.loggedIn);
+    }
+  }
+
+  _redirect(isLoggedIn) {
+    let route = this.props.location.pathname.slice(1);
+
+    if (['/', '/splash'].includes(route)) {
+      return;
+    }
+
+    if (isLoggedIn && ['signin', 'signup'].includes(route)) {
+      this.props.router.replace('home');
+    } else if (!isLoggedIn) {
+      this.props.router.replace('signin');
+    }
+  }
+
   render() {
+    console.log(this.props)
     return (
       <div className="base-wrapper">
         <div className="base-container">
@@ -26,8 +53,15 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => ({
+App.propTypes = {
+  loggedIn: PropTypes.bool.isRequired,
+  params: PropTypes.shape({
+    poolId: PropTypes.poolId
+  }).isRequired
+};
 
+const mapStateToProps = (state, ownProps) => ({
+  loggedIn: Boolean(state.user)
 });
 
 const mapDispatchToProps = dispatch => ({
