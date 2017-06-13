d06652979a2400a8f9b0e32d1d6c6cbf41ca6f7a
David Hu <davidhu314@gmail.com>
2017-06-13 15:50:37 -0700

    create confirm email method

--- app/mailers/auth_mailer.rb
+++ app/mailers/auth_mailer.rb
@@ -1,2 +1,8 @@
 class AuthMailer < ApplicationMailer
+
+  def confirm_email(user)
+    @user = user
+    @url = "#{root_url}/#/auth/confirm-email?token=#{user.confirm_token}"
+    mail(to: @user.email, subject: "Confirm your account to My Online Pool!")
+  end
 end
