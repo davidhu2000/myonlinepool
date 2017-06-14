78afebb769010b0eb44c658138509abeca84e843
Alex Sherman <asherman.ca@gmail.com>
2017-06-14 12:43:28 -0700

    changed made pick affordance

--- frontend/modules/picks/subcomponents/pick_form.jsx
+++ frontend/modules/picks/subcomponents/pick_form.jsx
@@ -36,26 +36,25 @@ class PickForm extends React.Component {
     let away = document.getElementById(this.props.Game.away);
     let home = document.getElementById(this.props.Game.home);
     if (pick === "away") {
-      away.classList.remove('pick-button');
+      // away.classList.remove('pick-button');
       away.classList.add('selected-button');
       home.classList.remove('selected-button');
-      home.classList.add('pick-button');
+      // home.classList.add('pick-button');
     } else {
-      home.classList.remove('pick-button');
+      // home.classList.remove('pick-button');
       home.classList.add('selected-button');
       away.classList.remove('selected-button');
-      away.classList.add('pick-button');
+      // away.classList.add('pick-button');
     }
   }
 
   render() {
     return (
       <div className="selection-item">
-        <label className="selection-form-away">
+        <label className="selection-form-away" id={this.props.Game.away}>
           <button onClick={() => this.submitPick("away")} />
           <img
             className="pick-button"
-            id={this.props.Game.away}
             src={`assets/logos/${this.props.Game.away}.gif`} 
           />
           <div className="selection-form-away-name">
@@ -66,12 +65,11 @@ class PickForm extends React.Component {
         <div className="selection-form-time">{this.state.time}</div>
         <div className="selection-form-line">{this.state.line}</div>
         <div className="selection-form-over">{this.state.over_under}</div>
-        <label className="selection-form-home">
+        <label className="selection-form-home" id={this.props.Game.home}>
           <div className="selection-form-home-name">{this.state.home}</div>
           <button onClick={() => this.submitPick("home")} />
           <img
             className='pick-button'
-            id={this.props.Game.home}
             src={`assets/logos/${this.props.Game.home}.gif`}
           />
         </label>
