ecee46954c9d4993c6cf4fda23e9c9b6b24db192
David Hu <davidhu314@gmail.com>
2017-06-15 12:15:56 -0700

    add helper to convert messages into objects

--- frontend/helpers/index.js
+++ frontend/helpers/index.js
@@ -1,2 +1,12 @@
 export * from './dropdown';
 export * from './validation_hoc';
+
+export const processMessages = (array, statusCode) => {
+  let type = 'error';
+
+  if (statusCode >= 200 && statusCode < 300) {
+    type = 'success';
+  }
+
+  return array.map(alert => ({ type, alert }));
+};
