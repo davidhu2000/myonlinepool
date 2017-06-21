4b465a3219279128ba14935d982d7e948f258e1e
David Hu <davidhu314@gmail.com>
2017-06-20 18:22:12 -0700

    create bulletin controller

---
+++ app/controllers/api/bulletins_controller.rb
@@ -0,0 +1,20 @@
+class Api::BulletinsController < ApplicationController
+  def index
+    @bulletins = Bulletin.where(pool_id: params[:pool_id]).order(id: :desc).limit(20).offset(params[:offset] || 0)
+  end
+
+  def create
+    @bulletin = Bulletin.new(bulletin_params)
+    @bulletin.user_id = current_user.id 
+
+    if @bulletin.save
+      render 'api/bulletins/show'
+    else 
+      render json: @bulletin.errors.full_messages, status: 422
+    end
+  end
+
+  def bulletin_params 
+    params.require(:bulletin).permit(:pool_id, :body)
+  end
+end
