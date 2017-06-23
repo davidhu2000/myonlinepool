8042c12da6bafbba68d507ed344cfffe3fc5b7df
David Hu <davidhu314@gmail.com>
2017-06-23 14:08:41 -0700

    add week result nfl model and migration

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
   def root
-    EvaluatePicksJob.perform_now(2016, 1)
+    EvaluatePicksJob.perform_now(2016, 2)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623191511) do
+ActiveRecord::Schema.define(version: 20170623210543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,4 +129,18 @@ ActiveRecord::Schema.define(version: 20170623191511) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
+  create_table "weekly_result_nfls", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "pool_id"
+    t.integer  "week"
+    t.integer  "season"
+    t.integer  "correct_picks"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["pool_id"], name: "index_weekly_result_nfls_on_pool_id", using: :btree
+    t.index ["user_id"], name: "index_weekly_result_nfls_on_user_id", using: :btree
+  end
+
+  add_foreign_key "weekly_result_nfls", "pools"
+  add_foreign_key "weekly_result_nfls", "users"
 end
