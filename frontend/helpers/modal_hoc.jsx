2bac4cfda4b5f4a7a50224edadfffe128f9ca621
David Hu <davidhu314@gmail.com>
2017-06-23 20:54:34 -0700

    create modal HOC

---
+++ frontend/helpers/modal_hoc.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Modal from 'react-modal';
+import PropTypes from 'prop-types';
+import autoBind from 'react-autobind';
+
+const customStyles = {
+  "content": {
+    "top": "50%",
+    "left": "50%",
+    "right": "auto",
+    "bottom": "auto",
+    "marginRight": "-50%",
+    "transform": "translate(-50%, -50%)",
+    "padding": "40px",
+    "borderRadius": "2px",
+    "boxShadow": "0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)",
+    "width": "400px"
+  }
+};
+
+export function withModal(Component) {
+  class WithModal extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        modalIsOpen: false
+      };
+
+      autoBind(this);
+    }
+
+    openModal() {
+      this.setState({ modalIsOpen: true });
+    }
+
+    closeModal() {
+      this.setState({ modalIsOpen: false });    
+    }
+
+    render() {
+      return (
+        <Modal
+          parentSelector={document.querySelector('#root')}
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel="label"
+          style={customStyles}
+        >
+          <Component {...this.props} />
+        </Modal>
+      );
+    }
+  }
+
+  return WithModal;
+}
