3d4b3108b5241f796f26f393574debb3c3d7146d
David Hu <davidhu314@gmail.com>
2017-06-26 16:50:07 -0700

    update picks controller to use api call to get current time

--- app/controllers/api/picks_controller.rb
+++ app/controllers/api/picks_controller.rb
@@ -1,6 +1,10 @@
+require 'net/http'
+require 'json'
+
 class Api::PicksController < ApplicationController
   def index 
     records = Team.calculate_team_records(2016)
+    current_time = DateTime.parse(get_current_time["currentDateTime"])
 
     all_games = GameNfl.where(season: 2016, week: params[:week]).includes(:home, :away)
     raw_picks = current_user.picks.where(pool_id: params[:poolId], game_id: all_games)
@@ -18,7 +22,8 @@ class Api::PicksController < ApplicationController
       @picks[game.id][:away_wins] = records[game.away_id][:wins]
       @picks[game.id][:away_losses] = records[game.away_id][:losses]
       @picks[game.id][:away_ties] = records[game.away_id][:ties]
-    end 
+      @picks[game.id][:pick_locked] = game.start_time < current_time
+    end
     raw_picks.each do |pick| 
       @picks[pick[:game_id]][:pick] = pick.pick 
     end
@@ -41,5 +46,13 @@ class Api::PicksController < ApplicationController
       @picks[game[:game_id]] = { game_id: pick.game_id, pool_id: pick.pool_id, pick: pick.pick }
     end
     render 'api/picks/index'
+  end
+
+  private 
+
+  def get_current_time
+    url = 'http://worldclockapi.com/api/json/utc/now'
+    uri = URI(url)
+    JSON.parse(Net::HTTP.get(uri))
   end
 end
