0a629637cc4b3517f992be7b1892d604d0ee83f4
David Hu <davidhu314@gmail.com>
2017-06-26 14:58:36 -0700

    format standings queries

--- app/controllers/api/standings_controller.rb
+++ app/controllers/api/standings_controller.rb
@@ -6,19 +6,15 @@ class Api::StandingsController < ApplicationController
   end
 
   def get_weekly_leaders(season, week)
-    @weekly_standings = WeeklyResultNfl.where(season: season, week: week).order(correct_picks: :desc).limit(10).includes(:pool, :user)
+    WeeklyResultNfl.where(season: season, week: week)
+      .includes(:pool, :user)
+      .order(correct_picks: :desc)
+      .limit(10)
   end
 
-  # def get_season_leaders(season)
-  #   WeeklyResultNfl.where(season: season).joins(:user, :pool)
-  #     .select('users.name as user_name', 'pools.title as pool_name', 'SUM(correct_picks) as correct_picks', 'SUM(wrong_picks) as wrong_picks')
-  #     .group('user_name', 'pool_name')
-  #     .order('correct_picks DESC')
-  #     .limit(10)
-  # end
-
   def get_season_leaders(season)
-    WeeklyResultNfl.where(season: season).includes(:user, :pool)
+    WeeklyResultNfl.where(season: season)
+      .includes(:user, :pool)
       .select(:user_id, :pool_id, 'SUM(correct_picks) as correct_picks', 'SUM(wrong_picks) as wrong_picks')
       .group(:user_id, :pool_id)
       .order('correct_picks DESC')
