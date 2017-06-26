36218aa1a17a98060d6e0b303c84cd5d3b7b98d7
David Hu <davidhu314@gmail.com>
2017-06-26 16:02:16 -0700

    update picks controller to use calculate_team_records method

--- app/models/team.rb
+++ app/models/team.rb
@@ -21,7 +21,7 @@ class Team < ApplicationRecord
   def self.calculate_team_records(season)
     games = GameNfl.where(season: season, completed: true)
     
-    records = Hash.new { |h, k| h[k] = { wins: 0, loses: 0, ties: 0 } }
+    records = Hash.new { |h, k| h[k] = { wins: 0, losses: 0, ties: 0 } }
 
     games.each do |game|
       winner = game.home_score <=> game.away_score
@@ -29,13 +29,13 @@ class Team < ApplicationRecord
       case winner
         when 1
           records[game.home_id][:wins] += 1
-          records[game.away_id][:loses] += 1
+          records[game.away_id][:losses] += 1
         when 0
           records[game.home_id][:ties] += 1
           records[game.away_id][:ties] += 1
         when -1
           records[game.away_id][:wins] += 1
-          records[game.home_id][:loses] += 1
+          records[game.home_id][:losses] += 1
       end
     end
 
