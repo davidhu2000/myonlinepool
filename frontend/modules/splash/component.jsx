75cb3ef28794c6b54fb8635499b05ed783a2ea31
David Hu <davidhu314@gmail.com>
2017-06-28 16:03:57 -0700

    randomize splash page logos on load

--- frontend/modules/splash/component.jsx
+++ frontend/modules/splash/component.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router';
+import { sampleSize } from 'lodash';
+import NflTeams from './nfl-teams.json';
 
 class Splash extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
+    console.log(this.props);
     return (
       <div className="splash-container">
         <div className="splash-top">
@@ -19,13 +17,14 @@ class Splash extends React.Component {
         </div>
         <div className="splash-mid">
           <h1>Play with your favorite teams</h1>
+
+          {/* Randomly display logo for 5 NFL teams */}
           <div className="splash-mid-carousel">
-            <div className='logo-Raiders' />
-            <div className='logo-Cardinals' />
-            <div className='logo-Bears' />
-            <div className='logo-Bills' />
-            <div className='logo-Panthers' />
+            { sampleSize(NflTeams, 5).map(name => (
+              <div key={name} className={`logo-${name}`} />
+            ))}
           </div>
+
         </div>
         <div className="splash-bottom">
           <h1>Create or join pools with all of your groups</h1>
@@ -45,4 +44,4 @@ class Splash extends React.Component {
   }
 }
 
-export default withRouter(Splash);
+export default Splash;
