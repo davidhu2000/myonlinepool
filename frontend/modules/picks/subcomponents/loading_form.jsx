24ec784d6f378e9a8ce35d0270adce0b9b524d85
David Hu <davidhu314@gmail.com>
2017-06-28 11:38:06 -0700

    update formatting for loading form

--- frontend/modules/picks/subcomponents/loading_form.jsx
+++ frontend/modules/picks/subcomponents/loading_form.jsx
@@ -1,71 +1,45 @@
 import React from 'react';
 
-class LoadingForm extends React.Component {
-  render() {
-    return (
-      <div className="selection-item">
-        <label className={'selection-form-away no-hover'} htmlFor='away-pick'>
-          <img
-            className="pick-button pick-away-button pending"
-            alt='TTT'
-          />
-          <div className="selection-form-away-name">
-            <div className='pending'>
-              {'TTTTTTTT'}
-            </div>
-            <div className='pending'>
-              {'TT'}-{'TT'}-{'TT'}
-            </div>
-          </div>
-        </label>
+const LoadingForm = () => {
+  return (
+    <div className="selection-item">
+      <label className={'selection-form-away no-hover'} htmlFor='away-pick'>
+        <img className="pick-button pick-away-button pending" alt='TTT' />
+        <div className="selection-form-away-name">
+          <div className='pending'>{'TTTTTTTT'}</div>
+          <div className='pending'>{'TT'}-{'TT'}-{'TT'}</div>
+        </div>
+      </label>
 
-        <div className="selection-form-time">
-          <div className="time-row">
-            <div className='pending'>
-              {'TTTT'}
-            </div>
-            <div className='time-props pending'>
-              <div>{'TT'}</div>
-              <div>{'TT'}</div>
-            </div>
-          </div>
-          <div className="date-row pending">
-            {'TTTTTT'}
+      <div className="selection-form-time">
+        <div className="time-row">
+          <div className='pending'>{'TTTT'}</div>
+          <div className='time-props pending'>
+            <div>{'TT'}</div>
+            <div>{'TT'}</div>
           </div>
         </div>
-        <div className="selection-form-line">
-          <div className='pending'>
-            {'TTTT'}
-          </div>
-        </div>
-        <div className="selection-form-spread">
-          <div className='pending'>
-            {'TTTT'}
-          </div>
-        </div>
-        <div className="selection-form-score">
-          <div className='pending'>
-            {'TT-TT'}
-          </div>
-        </div>
-
-        <label className={'selection-form-home no-hover'} htmlFor='home-pick'>
-          <div className="selection-form-home-name">
-            <div className='pending'>
-              {'TTTTTTTT'}
-            </div>
-            <div className='pending'>
-              {'TT'}-{'TT'}-{'TT'}
-            </div>
-          </div>
-          <img
-            className='pick-button pick-home-button pending'
-            alt={`TTTT`}
-          />
-        </label>
+        <div className="date-row pending">{'TTTTTT'}</div>
       </div>
-    );
-  }
-}
+      <div className="selection-form-line">
+        <div className='pending'>{'TTTT'}</div>
+      </div>
+      <div className="selection-form-spread">
+        <div className='pending'>{'TTTT'}</div>
+      </div>
+      <div className="selection-form-score">
+        <div className='pending'>{'TT-TT'}</div>
+      </div>
+
+      <label className={'selection-form-home no-hover'} htmlFor='home-pick'>
+        <div className="selection-form-home-name">
+          <div className='pending'>{'TTTTTTTT'}</div>
+          <div className='pending'>{'TT'}-{'TT'}-{'TT'}</div>
+        </div>
+        <img className='pick-button pick-home-button pending' alt={`TTTT`} />
+      </label>
+    </div>
+  );
+};
 
 export { LoadingForm };
