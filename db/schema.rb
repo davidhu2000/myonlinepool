46261ae23d87e76714dc476a37da46f503e0826b
Alex Sherman <asherman.ca@gmail.com>
2017-07-03 14:59:30 -0700

    members have paid fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624000446) do
+ActiveRecord::Schema.define(version: 20170703215705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,10 +48,11 @@ ActiveRecord::Schema.define(version: 20170624000446) do
   end
 
   create_table "memberships", force: :cascade do |t|
-    t.integer  "pool_id",    null: false
-    t.integer  "user_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "pool_id",                    null: false
+    t.integer  "user_id",                    null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "paid",       default: false, null: false
     t.index ["pool_id", "user_id"], name: "index_memberships_on_pool_id_and_user_id", unique: true, using: :btree
   end
 
