c51943a326adfbfbd6e5fbddfb8e979f2882182f
David Hu <davidhu314@gmail.com>
2017-07-05 17:49:12 -0700

    fix bug where week switcher cannot go back when on week 22

--- frontend/modules/picks/component.jsx
+++ frontend/modules/picks/component.jsx
@@ -23,11 +23,15 @@ class Picks extends React.Component {
 
   updateWeek(dir) {
     let week = this.state.week + dir;
+    if (week === 21) {
+      week += dir;
+    }
+
     if (week < 1) {
       week = 1;
     }
 
-    if (week > 22 || week === 21) {
+    if (week > 22) {
       week = 22;
     }
 
