38dd47f7f7ff2e23dbc87f53ac559ad38fa6bd37
Alex Sherman <asherman.ca@gmail.com>
2017-07-13 13:45:49 -0700

    profile page

--- frontend/modules/profile/component.jsx
+++ frontend/modules/profile/component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import autoBind from 'react-autobind';
 import { withRouter } from 'react-router';
+import { FormTextInput } from 'common/components';
 
 class Profile extends React.Component {
   constructor(props) {
@@ -18,11 +19,59 @@ class Profile extends React.Component {
     this.setState({ email: this.props.user.email, username: this.props.user.name });
   }
 
+  update(field) {
+    return e => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <div>
-        {this.props.user.email}
-        {this.props.user.name}
+      <div className="profile-container">
+
+        <form onSubmit={this.submitForm} className="auth-form">
+
+          <FormTextInput
+            update={this.update}
+            value={this.state.username}
+            type='name'
+            field="name"
+            label='username'
+            errorMessage="Please enter a new username"
+          />
+
+          <input
+            id="form-submit-button"
+            type='submit'
+            className="button auth-form-button"
+            value='Update'
+          />
+
+        </form>
+
+        <form onSubmit={this.submitForm} className="auth-form">
+
+          <FormTextInput
+            update={this.update}
+            value={this.state.email}
+            type='name'
+            field="email"
+            label='email'
+            errorMessage="Please enter a new email"
+          />
+
+          <input
+            id="form-submit-button"
+            type='submit'
+            className="button auth-form-button"
+            value='Update'
+          />
+
+        </form>
       </div>
     );
   }
