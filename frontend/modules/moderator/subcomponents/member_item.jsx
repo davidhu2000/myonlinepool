a974d11864bb6afeb6b70dcab885f1f6d7fabe34
Alex Sherman <asherman.ca@gmail.com>
2017-07-19 12:02:23 -0700

    modal for member removal

--- frontend/modules/moderator/subcomponents/member_item.jsx
+++ frontend/modules/moderator/subcomponents/member_item.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import autoBind from 'react-autobind';
 import PropTypes from 'prop-types';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class MemberItem extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      modalIsOpen: false
+    };
+
     autoBind(this);
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({ modalIsOpen: false });
   }
 
   renderClass(paid) {
@@ -26,7 +54,36 @@ class MemberItem extends React.Component {
         <button onClick={() => this.props.removeMember(this.props.member.userId, this.props.pool.id)}>
           <i className="fa fa-times" aria-hidden="true" />
         </button>
+        <button onClick={() => this.openModal()}>
+          modal
+        </button>  
         <span>{ this.props.member.name }</span>
+        
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"        
+        >
+          <div className="confirm-form">
+            <div className="confirm-header">
+              Confirm member removal?
+            </div>
+            <div className="confirm-buttons">
+              <button
+                type="submit"
+                className="confirm-form-button confirm-button"
+              >
+                Confirm
+              </button>
+
+              <button className="confirm-form-button" onClick={() => this.closeModal()}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Modal>
       </div>
     );
   }
