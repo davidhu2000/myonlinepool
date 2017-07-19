a2db22e4f21070cb4a9552aedea4b92d934f0678
Alex Sherman <asherman.ca@gmail.com>
2017-07-19 11:06:54 -0700

    bulletin box spacing

--- frontend/modules/pool_home/subcomponents/bulletin_box.jsx
+++ frontend/modules/pool_home/subcomponents/bulletin_box.jsx
@@ -18,8 +18,8 @@ class BulletinBox extends React.Component {
       <div className="bulletin-box">
         <div className="bulletin-header">
           <h2>Pool: {this.props.title}</h2>
-          <h2>Moderator: {this.props.moderatorName}</h2>
           <h2>Buy In: ${this.props.buyIn}</h2>
+          <h2>Moderator: {this.props.moderatorName}</h2>
         </div>
         <div className="bulletin-container">
           {this.renderBulletins()}
