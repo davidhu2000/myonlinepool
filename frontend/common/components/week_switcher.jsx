59d324492de90df7003e869fbf7678e9f8518908
Alex Sherman <asherman.ca@gmail.com>
2017-08-01 17:02:42 -0400

    removed text var from weekswitcher

--- frontend/common/components/week_switcher.jsx
+++ frontend/common/components/week_switcher.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const WeekSwitcher = ({ week, updateWeek, text }) => {
+const WeekSwitcher = ({ week, updateWeek }) => {
   if (week === 21) {
     week = 22;
   }
