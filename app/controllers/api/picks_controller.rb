612def605a3fe549630e4ec99a5ebdb6c5d9e33b
Alex Sherman <asherman.ca@gmail.com>
2017-08-13 19:20:06 -0700

    fixed typo

--- app/controllers/api/picks_controller.rb
+++ app/controllers/api/picks_controller.rb
@@ -30,7 +30,7 @@ class Api::PicksController < ApplicationController
     current_time = get_current_time
     pool = Pool.find_by(id: params[:picks].values.first[:pool_id])
      
-    return render(json: ['Pool Locked'], status: 404) unless pool.paymentMade
+    return render(json: ['Pool Locked'], status: 404) unless pool.payment_made
 
     params[:picks].each do |key, game|
       @week ||= game[:week]
