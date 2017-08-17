d304e991d647e5a43f6e8fb7de95c009f549cbbb
Alex Sherman <asherman.ca@gmail.com>
2017-08-17 12:02:23 -0700

    mailer front connected to back

--- app/controllers/api/mailers_controller.rb
+++ app/controllers/api/mailers_controller.rb
@@ -1,6 +1,6 @@
 class Api::MailersController < ApplicationController
   def create 
     puts params
-    # InviteMailer.invite_user(username,email,id,password).deliver
-  end 
+    InviteMailer.invite_user(params[:username],params[:email],params[:id],params[:password]).deliver
+  end
 end
