8ae8a176a5596298949cd477d5af67a63d979621
Alex Sherman <asherman.ca@gmail.com>
2017-09-05 22:53:38 -0700

    faqs added

--- frontend/modules/faq/component.jsx
+++ frontend/modules/faq/component.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { faqs } from './faqs.json';
 
 class Faq extends React.Component {
 
   render() {
     return (
       <div className="faq">
-        faq
+        { faqs.map(item => (
+          <div className="faq-item">
+            <div className="title">
+              {item.title}
+            </div>
+            <div className="info">
+              {item.info}
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
