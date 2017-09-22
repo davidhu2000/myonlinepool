c3d5c7ffa17c790be96464c17d6c25d122785152
Alex Sherman <asherman.ca@gmail.com>
2017-09-22 12:49:37 -0700

    back buttons on profile screen and pool form

--- frontend/modules/profile/component.jsx
+++ frontend/modules/profile/component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import autoBind from 'react-autobind';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
+import { withRouter, Link } from 'react-router';
 import { FormTextInput } from 'common/components';
 
 class Profile extends React.Component {
@@ -89,6 +89,12 @@ class Profile extends React.Component {
           />
 
         </form>
+
+        <Link to={"/home"}>
+          <div className="button back-button">
+            Back
+          </div>
+        </Link>
       </div>
     );
   }
