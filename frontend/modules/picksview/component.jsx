0bfa49c96caa30242801b474aa1ef35e6a76a2d3
Alex Sherman <asherman.ca@gmail.com>
2017-09-27 11:00:42 -0700

    picksview responsive refactor final

--- frontend/modules/picksview/component.jsx
+++ frontend/modules/picksview/component.jsx
@@ -54,6 +54,7 @@ class Picksview extends React.Component {
   render() {
     return (
       <div className="picks-view">
+     
         <div className="picks-view-header">
           <div className="name-title">
             Player Name
@@ -72,6 +73,7 @@ class Picksview extends React.Component {
           </div>
           {this.renderColumns()}
         </div>
+     
         <Link to={`/pool/${this.props.poolId}/picks/${this.props.params.weekId[0]}`}>
           <div className="button back-button">
             Back
